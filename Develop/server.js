c8f84eeaa029ddab3631fc172baed99665ea6702
romeo <rdumlao07@gmail.com>
2023-11-09 18:52:08 -0500

    added uuid and middleware for parsing JSON

--- Develop/server.js
+++ Develop/server.js
@@ -5,3 +5,12 @@ const path = require ('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// npm package for generating unique ID
+const { v4: uuidv4 } = require('uuid');
+
+const db = require('./db/db.json')
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.static('public'));
