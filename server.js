d6a81e765b796e5adee6bfcb69af2fc4e409502c
romeo <rdumlao07@gmail.com>
2023-11-09 20:28:44 -0500

    added get route '/'

--- server.js
+++ server.js
@@ -64,6 +64,11 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET route using '/' for index.html
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 // GET route for index.html
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
