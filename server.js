02e9cdd58bdb2a688421b4eea99f8030fcb62606
romeo <rdumlao07@gmail.com>
2023-11-09 20:10:12 -0500

    updated static files

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = require('./db/db.json')
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 // GET /api/notes route
