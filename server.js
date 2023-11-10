3eb478404de953103ca51253168654832283a4ec
romeo <rdumlao07@gmail.com>
2023-11-09 20:38:36 -0500

    updated app.listen

--- server.js
+++ server.js
@@ -74,5 +74,6 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.listen(PORT, () =>
-    console.log(`App listening on ${PORT}`))
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
